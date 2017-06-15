d48243375b4e7e14d7ee1b8d19819c2af6399de7
Peter Simonsson <peter@simonsson.com>
2017-06-15 12:01:26 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,133 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	const generalDecoratorType: vscode.DecorationRenderOptions = {
+		overviewRulerColor: 'rgba(255, 0, 0, 0.4)',
+		backgroundColor: 'rgba(255, 0, 0, 0.25)',
+		borderRadius: '1px',
+		borderColor: 'rgba(255, 0, 0, 0.0)',
+		borderStyle: 'solid',
+		borderSpacing: '2px',
+		borderWidth: '1px',
+	};
+
+	const trailingWhitespaceDecoratorType = vscode.window.createTextEditorDecorationType(
+		Object.assign({}, generalDecoratorType, {
+			overviewRulerLane: vscode.OverviewRulerLane.Right,
+		}),
+	);
+
+	const mixedIndentationDecoratorType = vscode.window.createTextEditorDecorationType(
+		Object.assign({}, generalDecoratorType, {
+			overviewRulerLane: vscode.OverviewRulerLane.Left,
+		}),
+	);
+
+	let activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(
+		editor => {
+			activeEditor = editor;
+			if (editor) {
+				triggerUpdateDecorations();
+			}
+		},
+		null,
+		context.subscriptions,
+	);
+
+	vscode.workspace.onDidChangeTextDocument(
+		event => {
+			if (activeEditor && event.document === activeEditor.document) {
+				triggerUpdateDecorations();
+			}
+		},
+		null,
+		context.subscriptions,
+	);
+
+	var timeout = null;
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(updateDecorations, 250);
+	}
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+
+		const trailingWhitespace = updateTrailingWhitespace();
+		const mixedIndentation = updateMixedIndentation();
+
+		activeEditor.setDecorations(
+			trailingWhitespaceDecoratorType,
+			trailingWhitespace,
+		);
+		activeEditor.setDecorations(
+			mixedIndentationDecoratorType,
+			mixedIndentation,
+		);
+	}
+
+	function updateTrailingWhitespace(
+		extraWhitespace: vscode.DecorationOptions[] = [],
+	) {
+		const regEx = /([ \t\f\v]+)$/gm;
+		const text = activeEditor.document.getText();
+
+		let match;
+		while ((match = regEx.exec(text))) {
+			const startIndex: number = match.index;
+			const endIndex: number = match.index + match[1].length;
+			const startPos = activeEditor.document.positionAt(startIndex);
+			const endPos = activeEditor.document.positionAt(endIndex);
+			const cursorFromEnd = activeEditor.selection.active.compareTo(endPos);
+
+			if (startIndex < endIndex && cursorFromEnd !== 0) {
+				const decoration = {
+					range: new vscode.Range(startPos, endPos),
+					hoverMessage: 'Unnecessary whitespace',
+				};
+				extraWhitespace.push(decoration);
+			}
+		}
+
+		return extraWhitespace;
+	}
+
+	function updateMixedIndentation(
+		extraWhitespace: vscode.DecorationOptions[] = [],
+	) {
+		const regEx = /^(([ ]+[\t]+)|([\t]+[ ]+))/gm;
+		const text = activeEditor.document.getText();
+
+		let match;
+		while ((match = regEx.exec(text))) {
+			const startIndex: number = match.index;
+			const endIndex: number = match.index + match[1].length;
+			const startPos = activeEditor.document.positionAt(startIndex);
+			const endPos = activeEditor.document.positionAt(endIndex);
+			const cursorFromEnd = activeEditor.selection.active.compareTo(endPos);
+
+			if (startIndex < endIndex && cursorFromEnd !== 0) {
+				const decoration = {
+					range: new vscode.Range(startPos, endPos),
+					hoverMessage: 'Mixed indentation',
+				};
+				extraWhitespace.push(decoration);
+			}
+		}
+
+		return extraWhitespace;
+	}
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
